b8e6eaab338d3418524bf648092ac8c108f3e9e7
Evan Dieterich <evan.dieterich.az@gmail.com>
2021-05-29 09:02:06 -0700

    MVP complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import "./App.css";
 
 function App() {
+  const [data , setData] = useState("")
+  useEffect(()=>{
+   axios
+   .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+   .then(res=>{
+     setData(res.data)
+   })
+   .catch(err=>{
+     console.log(err)
+   })
+  },[])
+
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <div className='App'>
+       <div className = 'Header'>
+          <h1>Nasa POTD</h1>
+       </div>
+       <img src={data.url} alt={data.title}/>
     </div>
   );
 }
