2245562e3436bfa4576dc53907eccfc4aee07d94
Evan Dieterich <evan.dieterich.az@gmail.com>
2021-05-30 08:39:32 -0700

    evan-dieterich

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
+import styled from 'styled-components';
 import "./App.css";
 
 function App() {
+
+  const Background = styled.body`
+  background-color: blue;
+  margin: 0 auto;
+`
+  const TitleStyle = styled.div`
+  font-family: 'Helvetica',
+  color: white;
+  border: 1px solid white;
+`
+
   const [data , setData] = useState("")
+
   useEffect(()=>{
    axios
    .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
@@ -17,9 +30,11 @@ function App() {
 
   return (
     <div className='App'>
+      <Background>
        <div className = 'Header'>
-          <h1>Nasa POTD</h1>
+          <TitleStyle><h1>Nasa POTD</h1></TitleStyle>
        </div>
+      </Background>
        <img src={data.url} alt={data.title}/>
     </div>
   );
